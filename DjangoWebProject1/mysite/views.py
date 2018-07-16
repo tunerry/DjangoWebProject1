7c68c26804edc68ccd00f74834639b2843f9c2dd
lv <781262125@qq.com>
2018-07-16 23:17:16 +0800

    some sample html use in django

--- DjangoWebProject1/mysite/views.py
+++ DjangoWebProject1/mysite/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 
-def index(request):
+user_list = [
+    {"user":"flansk","pwd":"22002"},
+    ]
 
+
+def index(request):
     #return HttpResponse("Hello World!")
-    return render(request, "index.html")
+    if request.method == "POST":
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+        user = {"user":username,"pwd":password}
+        user_list.append(user)
+    return render(request, "index.html",{"users":user_list})
